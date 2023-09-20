83d8598aaddbd5e06cbc3e458c7ff9440f2e67ca
Андрей Карпов <karpov.andrei.000@mail.ru>
2023-09-20 19:58:31 +0500

    Task1

--- Tyuiu.KarpovAA.Sprint1.Task1.V1.Lib/DataService.cs
+++ Tyuiu.KarpovAA.Sprint1.Task1.V1.Lib/DataService.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
 using tyuiu.cources.programming.interfaces.Sprint1;
 
 namespace Tyuiu.KarpovAA.Sprint1.Task1.V1.Lib
