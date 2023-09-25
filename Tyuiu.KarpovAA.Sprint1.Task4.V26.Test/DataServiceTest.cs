eeb2247075b2b706e36bcfc024ea616e9f9356d3
Андрей Карпов <karpov.andrei.000@mail.ru>
2023-09-25 21:12:07 +0500

    Task4

--- Tyuiu.KarpovAA.Sprint1.Task4.V26.Test/DataServiceTest.cs
+++ Tyuiu.KarpovAA.Sprint1.Task4.V26.Test/DataServiceTest.cs
@@ -13,7 +13,7 @@ namespace Tyuiu.KarpovAA.Sprint1.Task4.V26.Test
             DataService ds = new DataService();
             double x = 2;
             double y = 2;
-            double wait = 0.125;
+            double wait = 0.057;
             var res = ds.Calculate(x, y);
             Assert.AreEqual(wait, res);
         }
