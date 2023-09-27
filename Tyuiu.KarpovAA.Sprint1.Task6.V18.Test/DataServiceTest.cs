ebf1af203237b086a728159e9f529335e04b3caf
Андрей Карпов <karpov.andrei.000@mail.ru>
2023-09-27 13:33:59 +0500

    Task6

---
+++ Tyuiu.KarpovAA.Sprint1.Task6.V18.Test/DataServiceTest.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.KarpovAA.Sprint1.Task6.V18.Lib;
+
+namespace Tyuiu.KarpovAA.Sprint1.Task6.V18.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            string strTest = "1234";
+            DataService ds = new DataService();
+            bool res = ds.CheckNumber(strTest);
+            string wait = "true";
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.KarpovAA.Sprint1.Task6.V18/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.KarpovAA.Sprint1.Task6.V18.Lib;
+
+namespace Tyuiu.KarpovAA.Sprint1.Task6.V18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #1 | Выполнил: Карпов А. А. | ИИПб-23-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #1                                                               *");
+            Console.WriteLine("* Тема: Работа со строками класс String                                   *");
+            Console.WriteLine("* Задание #6                                                              *");
+            Console.WriteLine("* Вариант #18                                                             *");
+            Console.WriteLine("* Выполнил: Карпов Андрей Андреевич | ИИПб-23-1                           *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу: пользователь вводит текст. Проверить, что строка    *");
+            Console.WriteLine("* является символьным представлением натурального числа '122', '34' и т.д.*");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            int res;
+
+            if 
+                 (Int32.TryParse(Console.ReadLine(), out res))
+                 Console.WriteLine("* Введенная строка является символьным представлением натурального числа. *");
+            else 
+                 Console.WriteLine("* Введенная строка не является символьным представлением натурального числа. *");
+                 Console.ReadKey();
+        }
+    }
+}
+
